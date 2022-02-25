22086abf88c3b2e383971c8e3df2993b90d29ac0
Marco Pascucci <36882726+mpascucci@users.noreply.github.com>
2022-02-25 11:37:22 +0100

    Dev update (#1)
    * import multipagetiff made transparent
    
    * use Stack in PSF input instead of path
    
    * new readme

--- pycroscopy/__init__.py
+++ pycroscopy/__init__.py
@@ -1,2 +1,7 @@
 # from .deconvolution import deconvolve
 from .psf import PSF
+
+import multipagetiff
+from multipagetiff.io import *
+from multipagetiff.plot import *
+from multipagetiff.stack import Stack
